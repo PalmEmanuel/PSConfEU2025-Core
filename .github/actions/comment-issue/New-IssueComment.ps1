d568939e06310baf55e273681b82951ed2363ef3
Emanuel Palm <emanuel+github@pipe.how>
2025-06-22 16:17:50 +0200

    working demo 1

--- .github/actions/comment-issue/New-IssueComment.ps1
+++ .github/actions/comment-issue/New-IssueComment.ps1
@@ -1,15 +1,30 @@
 param(
-	# Comment Params
+	[Parameter(Mandatory)]
+	[string]$Token,
+
+	[Parameter(Mandatory)]
+	[string]$IssueNumber,
+
+	[Parameter(Mandatory)]
+	[string]$Body,
+
+	[Parameter(Mandatory)]
+	[string]$Repository
 )
 
 $Headers = @{
-    # Comment Headers
+    Accept = 'application/vnd.github+json'
+	Authorization = "Bearer $Token"
 }
 
-# Comment Body
+$Body = @{ body = $Body } | ConvertTo-Json
 
 $Params = @{
-    # Comment Splat Params
+    Uri = "https://api.github.com/repos/$Repository/issues/$IssueNumber/comments"
+	Headers = $Headers
+	Method = 'Post'
+	Body = $Body
+	ContentType = 'application/json'
 }
 
 Invoke-RestMethod @Params
